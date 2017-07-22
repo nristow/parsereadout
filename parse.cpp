d42cdd41c861bd157971cced51ead4461e5bc48f
Nick Ristow <nick.ristow@gmail.com>
2017-07-22 16:37:15 -0400

    initial commit

---
+++ parse.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+int main()
+{
+
+	return 0;
+}
+
+std::vector<std::string> getNextLineAndSplitIntoTokens(std::istream& str)
+{
+    std::vector<std::string>   result;
+    std::string                line;
+    std::getline(str,line);
+
+    std::stringstream          lineStream(line);
+    //lineStream << line;
+    std::string                cell;
+
+    while(std::getline(lineStream,cell, ','))
+    {
+        result.push_back(cell);
+    }
+    // This checks for a trailing comma with no data after it.
+    if (!lineStream && cell.empty())
+    {
+        // If there was a trailing comma then add an empty element.
+        result.push_back("");
+    }
+    return result;
+}
